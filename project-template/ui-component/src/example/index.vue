9ec516b672a33cd543461ad8f5c7f21ced1aeb45
hoperyy <liuyuanyang@weidian.com>
2019-02-07 20:38:45 +0800

    update

---
+++ project-template/ui-component/src/example/index.vue
@@ -0,0 +1,33 @@
+
+<template>
+<div class="index-container">
+    <wd-header title="WDUI Components"></wd-header>
+    <wd-cell :title="item" :to="`#/${item.toLowerCase()}`" v-for="item in components" :key="item"></wd-cell>
+</div>
+</template>
+
+<script>
+// This file is created by "npm run build:example-route"
+export default {
+    data() {
+        return {
+            components: [
+                
+        'Button',
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="sass">
+body {
+    font-family: Arial, "Microsoft YaHei";
+
+    .iconfont.icon-zuosanjiao {
+        font-size: 36px;
+        // color: #fff;
+        color: #000;
+    }
+}
+</style>
---
+++ project-template/ui-component/src/website/ui-package-info.js
@@ -0,0 +1,6 @@
+
+            export default {
+                name: '@vdian/wdui-project',
+                description: 'demo 组件库'
+            };
+        
