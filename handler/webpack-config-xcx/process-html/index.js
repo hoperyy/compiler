d9e7743859f6a3228e4facb58de043ea494e3267
hoperyy <antfootmail@gmail.com>
2019-02-27 00:12:36 +0800

    feat: support wx

--- handler/webpack-config-xcx/process-html/index.js
+++ handler/webpack-config-xcx/process-html/index.js
@@ -26,11 +26,11 @@ function buildHtml(finalConfig) {
     let pagesDir = utilGetPageDir(srcDir);
 
     if (!pagesDir) {
-        logUtil.error('\n\n项目中找不到页面入口 wxml 文件\n\n');
+        logUtil.error('\n\n项目中找不到页面入口 html 文件\n\n');
         return;
     }
 
-    let stream = gulp.src([`!${path.join(pagesDir, '/**/*.tpl.html')}`, path.join(pagesDir, '/**/*.wxml')]);
+    let stream = gulp.src([path.join(pagesDir, '/**/*.html')]);
 
     if (replace) {
         Object.keys(replace).forEach((key) => {
@@ -42,22 +42,22 @@ function buildHtml(finalConfig) {
 
     let pipeline = null;
 
+    pipeline = stream.pipe(rename((_path) => {
+        _path.basename = 'index';
+        _path.dirname = _path.dirname;
+        _path.extname = '.wxml';
+    }));
+
     if (/build/.test(taskName)) {
-        pipeline = stream.pipe(rename((_path) => {
-            _path.basename = _path.dirname;
-            _path.dirname = '';
-        })).pipe(htmlmin({
+        pipeline = pipeline.pipe(htmlmin({
             minifyJS: true,
             minifyCSS: true,
             collapseWhitespace: true,
             removeComments: false
-            })).pipe(gulp.dest(buildPagesDir));
-    } else {
-        pipeline = stream.pipe(rename((_path) => {
-            _path.basename = 'index';
-            _path.dirname = _path.dirname;
-        })).pipe(gulp.dest(buildPagesDir));
+        }));
     }
+
+    pipeline = pipeline.pipe(gulp.dest(buildPagesDir));
     
     // build 目录的 html 文件生成后，添加一些通用脚本，比如埋点、jsbridge、flexible 等
     pipeline.on('end', () => {
@@ -86,7 +86,7 @@ function presetHtml(finalConfig) {
     const pageDir = utilGetPageDir(srcDir);
 
     if (watch) {
-        return htmlWatcher = gulp.watch([path.join(pageDir, '/**/*.wxml')], () => {
+        return htmlWatcher = gulp.watch([path.join(pageDir, '/**/*.html')], () => {
             buildHtml(finalConfig);
         });
     }
